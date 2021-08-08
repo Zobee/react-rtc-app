d42848ea4387c063c87e193b4c57280570cb3934
Zobee <zohebboga@hotmail.com>
2021-08-07 21:02:07 -0400

    Removed unused dependency

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import Login from "./components/Login/Login";
+import Dashboard from "./components/Dashboard";
 import useLocalStorage from "./hooks/useLocalStorage";
 
 const App:React.FC = () => {
   const [id, setId] = useLocalStorage("key")
   return (
     <div className="App">
-      <h1>{id}</h1>
-      <Login setId={setId} />
+      {id ? <Dashboard /> : <Login setId={setId} />}
     </div>
   );
 }
